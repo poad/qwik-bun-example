febec974c597583b2be01e6f55db61b99143354b
Kenji Saito <ken-yo@mbr.nifty.com>
2024-04-03 08:57:58 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -2,14 +2,14 @@
 
 CUR=$(pwd)
 
-CURRENT=$(cd $(dirname $0);pwd)
+CURRENT=$(cd "$(dirname $0)" || exit;pwd)
 echo "${CURRENT}"
 
-cd "${CURRENT}"
+cd "${CURRENT}" || exit
 git pull --prune
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
 echo ""
@@ -17,14 +17,14 @@ pwd
 rm -rf node_modules && pnpm install && pnpm up && rm -rf node_modules pnpm-lock.yaml && bun install && bun update && bun lint-fix
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
 git commit -am "Bumps node modules" && git push
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
 
-cd "${CUR}"
+cd "${CUR}" || exit
